f8e50cedb5ab3a15300b94bf03cafa1a7934cba7
David Hinwood <david.hinwood@canberra.edu.au>
2021-11-12 17:51:39 +1100

    Update refresh.bash

--- refresh.bash
+++ refresh.bash
@@ -3,4 +3,4 @@
 ./kill.bash
 docker build . -t rpi_conf --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)"
 ./run_system.bash
-./run_hand
+./run_hand.bash
