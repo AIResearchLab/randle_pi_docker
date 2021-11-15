56b99106076cc1f6f009715069d535d92894b529
David Hinwood <david.hinwood@canberra.edu.au>
2021-11-16 10:04:43 +1100

    Update refresh.bash

--- refresh.bash
+++ refresh.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This file should be run from its directory
 ./kill.bash
-docker build . -t rpi_conf --platform linux/amd64 --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)"
+docker build . -t rpi_conf --platform linux/arm --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)"
 ./run_system.bash
 ./run_hand.bash
