75032c910c09a659878073c94e8a614a334c81dc
David Hinwood <david.hinwood@canberra.edu.au>
2021-11-16 13:23:56 +1100

    Update run_hand.bash

--- run_hand.bash
+++ run_hand.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -d rpi_controller bash -c "/home/baxter/catkin_ws/devel/env.sh roslaunch randle_serial run_hand.launch"
+docker exec -d rpi_controller bash -c "/home/baxter/catkin_ws/devel/env.sh roslaunch randle_serial run_hand.launch button_sensor:=false"
