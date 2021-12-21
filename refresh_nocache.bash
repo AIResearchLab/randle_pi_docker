f21c473e06406ee68c4e712069c503e49d193736
robodave94 <David.Hinwood@canberra.edu.au>
2021-12-21 18:36:32 +1100

    no cache bash file ran

---
+++ refresh_nocache.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This file should be run from its directory
+./kill.bash
+docker build . -t rpi_conf --no-cache --platform linux/arm64/v8 --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)"
+./run_system.bash
+./run_hand.bash
